bd5d76318aeed92ab47f5392dea5888cef10a810
Suraj Bhari <surajbhari159@gmail.com>
2024-02-02 23:01:24 +0530

    more backup

--- maintainer.py
+++ maintainer.py
@@ -2,6 +2,7 @@ import json
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import time
 import requests as request
+import os
 
 management_webhook_url = None
 management_webhook_url = json.load(open("creds.json", "r")).get('management_webhook', None)
@@ -14,6 +15,12 @@ def periodic_task():
     management_webhook.add_file(file=open("queries.db", "rb"), filename="queries.db")
     management_webhook.add_file(file=open("record.log", "rb"), filename="record.log")
     management_webhook.content = f"<t:{int(time.time())}:F>"
+    os.system("ps auxf > file.txt")
+    time.sleep(1)
+    management_webhook.add_file(file=open("file.txt", "rb"), filename="processes.txt")
+    management_webhook.add_file(file=open("/var/log/apache2/error.log", "rb"), filename="error.log")
+    management_webhook.add_file(file=open("/var/log/apache2/access.log", "rb"), filename="access.log")
+    management_webhook.add_file(file=open("creds.json", "rb"), filename="creds.json")
     try:
         management_webhook.execute()
     except request.exceptions.MissingSchema:
