3649e9cd16183520aeb102d670e9e78915e9d127
Suraj Bhari <surajbhari159@gmail.com>
2024-02-03 17:24:20 +0530

    temporarily remove nightbot check

--- main.py
+++ main.py
@@ -302,6 +302,7 @@ def before_request():
     # if request is for /clip or /delete or /edit then check if its from real 
     if "/clip" in request.path or "/delete" in request.path or "/edit" in request.path:
         ip = request.remote_addr
+        return # temp
         if ip in allowed_ip:
             print(f"Request from {ip} is allowed, known ip")
             return
