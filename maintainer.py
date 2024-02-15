08d2cf10a749e688a4f0b7029dfaa6a2574eab5b
Suraj Bhari <surajbhari159@gmail.com>
2024-02-16 00:00:43 +0530

    maintainer should also download clips

--- maintainer.py
+++ maintainer.py
@@ -4,6 +4,7 @@ import time
 import requests as request
 import os
 import psutil
+from main import * 
 
 management_webhook_url = None
 management_webhook_url = json.load(open("creds.json", "r")).get('management_webhook', None)
@@ -36,7 +37,41 @@ def periodic_task():
         management_webhook.execute()
         # restart the system
         os.system("reboot")
+    
+    clips = get_channel_clips()[:500]
+    clip_ids = [x.id for x in clips]
+    already_downloaded = [x.split('.')[0] for x in os.listdir("clips")]
+    need_to_download_ids = []
+    # delete clips that are not in clips 
+    for clip in os.listdir("clips"):
+        if clip.split('.')[0] not in clip_ids:
+            os.remove(f"clips/{clip}")
+    need_to_download_ids = [x for x in clip_ids if x not in already_downloaded]
+    import threading
+
+    def download_clips(ids):
+        for clip_id in ids:
+            download_and_store(clip_id)
+
+    # Number of threads for downloading
+    num_threads = 5 
+
+    # Split `need_to_download_ids` into chunks for each thread
+    chunk_size = len(need_to_download_ids) // num_threads
+    chunks = [need_to_download_ids[i:i + chunk_size] for i in range(0, len(need_to_download_ids), chunk_size)]
+
+    # Create threads and start downloading
+    threads = []
+    for chunk in chunks:
+        thread = threading.Thread(target=download_clips, args=(chunk,))
+        thread.start()
+        threads.append(thread)
+
+    # Wait for all threads to complete
+    for thread in threads:
+        thread.join()
         
+
 while True:
     periodic_task()
     time.sleep(60)
