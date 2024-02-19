ef5beb704851477b9aeefcec1541e8e3c3abd3ad
SurajBhari <surajbhari159@gmail.com>
2024-02-19 23:06:12 +0530

    headers can't have underscores in it

--- main.py
+++ main.py
@@ -1118,7 +1118,7 @@ def add():
                     "Nightbot-User": f"providerId={chat['author']['id']}&displayName={chat['author']['name']}&userLevel={user_level}",
                     "Nightbot-Response-Url": "https://api.nightbot.tv/1/channel/send/",
                     "videoID": vid_id,
-                    "time_in_seconds": str(chat["time_in_seconds"]),
+                    "time-in-seconds": str(chat["time_in_seconds"])
                 }
                 if request.is_secure:
                     htt = "https://"
@@ -1186,6 +1186,10 @@ def clip(message_id, clip_desc=None):
     except ValueError:
         silent = 2
     delay = request.args.get("delay", 0)
+    try:
+        delay = int(delay)
+    except ValueError:
+        return "Given Delay is not a number"
     show_link = False if show_link == "false" else True
     screenshot = True if screenshot == "true" else False
     try:
@@ -1223,13 +1227,16 @@ def clip(message_id, clip_desc=None):
     if not vid:
         return "No LiveStream Found."
     clip_time = request_time - vid["start_time"] / 1000000 + 5 
-    h_clip_time = request.headers.get("time_in_seconds") 
+    h_clip_time = request.headers.get("time-in-seconds")  
     if h_clip_time:
-        clip_time = h_clip_time
+        print(f"using clip time from headers {h_clip_time}")
+        try:
+            clip_time = float(h_clip_time)
+        except ValueError:
+            return "Given clip_time in headers is not a number."
     clip_time += delay
     url = "https://youtu.be/" + vid["original_video_id"] + "?t=" + str(int(clip_time))
     clip_id = message_id[-3:] + str(int(clip_time))
-
     # if clip_time is in seconds. then hh:mm:ss format would be like
     hour_minute_second = time_to_hms(clip_time)
     message_cc_webhook = (
